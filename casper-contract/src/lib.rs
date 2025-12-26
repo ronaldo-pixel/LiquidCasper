5ff20a8f193dd1757ecf5b13e20d56813e766e81
noname <noname@gmail.com>
2025-12-26 23:24:36 +0530

    testing delegation using contract

--- casper-contract/src/lib.rs
+++ casper-contract/src/lib.rs
@@ -3,17 +3,64 @@
 
 extern crate alloc;
 
-use alloc::string::String;
-use casper_contract::contract_api::{runtime, storage};
+use alloc::{vec, string::ToString};
+
+use casper_contract::contract_api::{runtime, storage, system};
+use casper_types::{
+    contracts::{
+        EntryPoint, EntryPointAccess, EntryPointType, EntryPoints, NamedKeys,
+    },
+    account::AccountHash,
+    runtime_args, RuntimeArgs,
+    CLType, Parameter, PublicKey, U512,
+};
+
+const ARG_AMOUNT: &str = "amount";
+const ARG_VALIDATOR: &str = "validator";
+
+#[no_mangle]
+pub extern "C" fn delegate_to_validator() {
+    let amount: U512 = runtime::get_named_arg(ARG_AMOUNT);
+    let validator: PublicKey = runtime::get_named_arg(ARG_VALIDATOR);
+
+    // Delegator MUST be the transaction signer
+    let delegator: AccountHash = runtime::get_caller();
+
+    let auction_hash = system::get_auction();
+
+    runtime::call_contract::<()>(
+        auction_hash,
+        "delegate",
+        runtime_args! {
+            "delegator" => delegator,
+            "validator" => validator,
+            "amount" => amount,
+        },
+    );
+}
 
 #[no_mangle]
 pub extern "C" fn call() {
-    // Create a new on-chain value
-    let greeting = String::from("Hello Casper");
+    let mut entry_points = EntryPoints::new();
 
-    // Store it in global state and get a URef
-    let greeting_ref = storage::new_uref(greeting);
+    entry_points.add_entry_point(EntryPoint::new(
+        "delegate_to_validator",
+        vec![
+            Parameter::new(ARG_AMOUNT, CLType::U512),
+            Parameter::new(ARG_VALIDATOR, CLType::PublicKey),
+        ],
+        CLType::Unit,
+        EntryPointAccess::Public,
+        EntryPointType::Contract,
+    ));
 
-    // Save it under a named key so it’s easy to find
-    runtime::put_key("greeting", greeting_ref.into());
+    let (contract_hash, _) = storage::new_contract(
+        entry_points,
+        Some(NamedKeys::new()),
+        Some("liquid_staking_contract".to_string()),
+        None,
+    );
+
+    runtime::put_key("liquid_staking_contract", contract_hash.into());
 }
+
